18a5a20906e2f2b7b53854b5ceedf8ea842720b1
iqzar <50322762+iqzar@users.noreply.github.com>
2019-05-20 01:47:20 -0700

    Whether no even or odd

---
+++ Task10.py
@@ -0,0 +1,7 @@
+#whether a given number id even or odd
+num =int(input("Enter any number: "))
+n= num % 2
+if n > 0 :
+    print("Entered no. is odd number: ", num)
+else :
+    print("Entered no. an even number: ", num)
