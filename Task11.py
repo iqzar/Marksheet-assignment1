bc04419b8185860810ae8ec5643018bfab742446
iqzar <50322762+iqzar@users.noreply.github.com>
2019-05-20 01:48:23 -0700

    letter is vowel or not

---
+++ Task11.py
@@ -0,0 +1,6 @@
+#Check whether entered letter is vowel or not :
+l =input("Enter a letter: ")
+if(l=="o" or l=="i" or l=="e" or l=="u" or l=="a" or l=="O" or l=="E" or l=="I" or l=="U" or l=="A"):
+    print("Entered letter is vowel:",l)
+else :
+     print("Entered letter is consonanat:", l)   
